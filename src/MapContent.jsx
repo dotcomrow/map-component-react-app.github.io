bf3aa42b990ea34581e92359452a6a127c1d8b32
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-09-21 16:54:22 -0400

    css fixes
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/MapContent.jsx
+++ src/MapContent.jsx
@@ -20,7 +20,7 @@ function MapContent({ mapIsReadyCallback, vars }) {
 
     return (
         <ErrorBoundary FallbackComponent={ErrorFallback}>
-            <div className="container-fluid bg-white p-2 h-100">
+            <div className="container-fluid bg-white p-2 h-100 d-flex flex-column">
                 <Select>Select from DTK</Select>
                 <MyMap mapIsReadyCallback={mapIsReadyCallback} vars={vars} />
             </div>
